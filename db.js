27e29cb76eb877b72324f66440d608f11ac26da4
Alerdo Ballabani <110819723+Alerdo@users.noreply.github.com>
2023-11-12 22:26:16 +0000

    Update Notes.txt

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool  = new Pool({
     user: "postgres",
     host: "localhost",
     database: "students",
-    password: "Haxhiballabani11",
+    password: "password",
     port: 5432
 });
 
